efd4c9c84e08d1714f7bc764f49266604354dba3
gilmarxd <gilmar_custodio_@hotmail.com>
2022-04-26 02:56:36 +0000

    ✨ enable validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3333, () => {
     Logger.log(`🚀 Server running on http://localhost:3333/`);
   });
