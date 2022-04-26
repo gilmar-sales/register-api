433cf16a8942862d5d987baaa6dd00c433963635
gilmarxd <gilmar_custodio_@hotmail.com>
2022-04-26 02:03:44 +0000

    🔊 Startup log

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3333, () => {
+    Logger.log(`🚀 Server running on http://localhost:3333/`);
+  });
 }
 bootstrap();
