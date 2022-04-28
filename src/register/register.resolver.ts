a93ea2f325622fd052fb3e87b96627ffe0889c6a
gilmarxd <gilmar_custodio_@hotmail.com>
2022-04-28 08:52:16 -0300

    🔒️ add auth guard to registers resolver

--- src/register/register.resolver.ts
+++ src/register/register.resolver.ts
@@ -3,11 +3,14 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { RegisterService } from './register.service';
 import { Register } from './register.entity';
 import CreateRegisterDTO from './dto/create-register.dto';
+import { AuthGuard } from '../auth/guard/auth.guard';
+import { UseGuards } from '@nestjs/common';
 
 @Resolver()
 export class RegisterResolver {
   constructor(private readonly registerService: RegisterService) {}
 
+  @UseGuards(new AuthGuard())
   @Mutation(() => Register)
   async createRegister(
     @Args('data') data: CreateRegisterDTO,
@@ -20,6 +23,7 @@ export class RegisterResolver {
     return this.registerService.findAllRegisters();
   }
 
+  @UseGuards(new AuthGuard())
   @Query(() => [Register])
   async findRegistersByUser(@Args('userId') userId: number) {
     return this.registerService.findRegistersByUser(userId);
