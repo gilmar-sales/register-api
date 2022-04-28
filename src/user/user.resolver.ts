f48ba581e308e907ba381cc3ed49f830b780f1d1
gilmarxd <gilmar_custodio_@hotmail.com>
2022-04-28 09:45:18 -0300

    🔒️ add guards to user resolver

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -3,21 +3,26 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { UserService } from './user.service';
 import { User } from './user.entity';
 import { CreateUserDTO } from './dto/create-user.dto';
+import { UseGuards } from '@nestjs/common';
+import { RoleGuard } from 'src/auth/guard/role.guard';
 
 @Resolver()
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
 
+  @UseGuards(RoleGuard)
   @Mutation(() => User)
   async createUser(@Args('data') data: CreateUserDTO): Promise<User> {
     return await this.userService.createUser(data);
   }
 
+  @UseGuards(RoleGuard)
   @Mutation(() => Boolean)
   async deleteUser(@Args('data') data: number): Promise<boolean> {
     return await this.userService.deleteUser(data);
   }
 
+  @UseGuards(RoleGuard)
   @Query(() => [User])
   async findAllUsers(): Promise<User[]> {
     return await this.userService.findAllUsers();
