f4e98bdc002144b9e24ff74d9d644a7d9d08047e
gilmarxd <gilmar_custodio_@hotmail.com>
2022-04-29 22:52:21 -0300

    ✨ add graphql query to get the user data by the authentication

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,10 +1,11 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
 
 import { UserService } from './user.service';
 import { User } from './user.entity';
 import { CreateUserDTO } from './dto/create-user.dto';
 import { UseGuards } from '@nestjs/common';
 import { RoleGuard } from 'src/auth/guard/role.guard';
+import { AuthGuard } from 'src/auth/guard/auth.guard';
 
 @Resolver()
 export class UserResolver {
@@ -20,6 +21,13 @@ export class UserResolver {
   @Mutation(() => Boolean)
   async deleteUser(@Args('data') data: number): Promise<boolean> {
     return await this.userService.deleteUser(data);
+  }
+
+  @UseGuards(AuthGuard)
+  @Query(() => User)
+  async getUser(@Context() context): Promise<User> {
+    const userId: number = context.req.user.id;
+    return await this.userService.findById(userId);
   }
 
   @UseGuards(RoleGuard)
