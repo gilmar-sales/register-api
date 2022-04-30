27574ba78d9145b1e462e8b0b582e4e90b18a679
gilmarxd <gilmar_custodio_@hotmail.com>
2022-04-30 14:59:58 -0300

    ✅ add register module tests

--- src/register/register.resolver.spec.ts
+++ src/register/register.resolver.spec.ts
@@ -1,18 +1,101 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RegisterResolver } from './register.resolver';
+import { RegisterService } from './register.service';
+import { User } from '../user/user.entity';
+import CreateRegisterDTO from './dto/create-register.dto';
+import { PubSub } from 'graphql-subscriptions';
 
 describe('RegisterResolver', () => {
   let resolver: RegisterResolver;
 
-  beforeEach(async () => {
+  const serviceMock = {
+    createRegister: jest.fn(),
+    findRegistersByUser: jest.fn(),
+    findAllRegisters: jest.fn(),
+  };
+
+  const pubSubMock = {
+    asyncIterator: jest.fn(),
+    publish: jest.fn(),
+  };
+
+  const mockData: CreateRegisterDTO = {
+    timeRegistered: new Date(),
+    type: 'in',
+  };
+
+  const contextMock = {
+    req: {
+      user: {
+        id: -1,
+      },
+    },
+  };
+
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RegisterResolver],
+      providers: [
+        RegisterResolver,
+        { provide: RegisterService, useValue: serviceMock },
+        { provide: PubSub, useValue: pubSubMock },
+      ],
     }).compile();
 
     resolver = module.get<RegisterResolver>(RegisterResolver);
   });
 
+  beforeEach(() => {
+    serviceMock.createRegister.mockReset();
+    serviceMock.findRegistersByUser.mockReset();
+    serviceMock.findAllRegisters.mockReset();
+  });
+
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  describe('when create a register', () => {
+    it('should return the created register', async () => {
+      serviceMock.createRegister.mockReturnValue({ ...mockData, id: -1 });
+
+      const user = new User();
+      user.id = -1;
+
+      const register = await resolver.createRegister(mockData, contextMock);
+
+      expect(register).toHaveProperty('id');
+      expect(register).toMatchObject(mockData);
+      expect(serviceMock.createRegister).toBeCalledWith(user.id, mockData);
+      expect(serviceMock.createRegister).toBeCalledTimes(1);
+    });
+  });
+
+  describe('when search register by user id', () => {
+    it('should find all the registers of a user by their id', async () => {
+      serviceMock.findRegistersByUser.mockReturnValue([mockData, mockData]);
+
+      const userId = -1;
+      const registers = await resolver.findRegistersByUser(userId);
+
+      expect(registers).toHaveLength(2);
+      expect(registers);
+      expect(serviceMock.findRegistersByUser).toBeCalledWith(userId);
+      expect(serviceMock.findRegistersByUser).toBeCalledTimes(1);
+    });
+  });
+
+  describe('when search all registers', () => {
+    it('should find all registers', async () => {
+      serviceMock.findAllRegisters.mockReturnValue([
+        mockData,
+        mockData,
+        mockData,
+      ]);
+
+      const registers = await resolver.findAllRegisters();
+
+      expect(registers).toHaveLength(3);
+      expect(serviceMock.findAllRegisters).toBeCalledTimes(1);
+    });
   });
 });
--- src/register/register.service.spec.ts
+++ src/register/register.service.spec.ts
@@ -1,18 +1,87 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import CreateRegisterDTO from './dto/create-register.dto';
+import { Register } from './register.entity';
 import { RegisterService } from './register.service';
 
 describe('RegisterService', () => {
   let service: RegisterService;
 
-  beforeEach(async () => {
+  const repositoryMock = {
+    create: jest.fn(),
+    save: jest.fn(),
+    find: jest.fn(),
+  };
+
+  const mockData: CreateRegisterDTO = {
+    timeRegistered: new Date(),
+    type: 'in',
+  };
+
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RegisterService],
+      providers: [
+        RegisterService,
+        { provide: getRepositoryToken(Register), useValue: repositoryMock },
+      ],
     }).compile();
 
     service = module.get<RegisterService>(RegisterService);
   });
 
+  beforeEach(() => {
+    repositoryMock.create.mockReset();
+    repositoryMock.save.mockReset();
+    repositoryMock.find.mockReset();
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('when create a register', () => {
+    it('should be create a employee', async () => {
+      repositoryMock.create.mockReturnValue(mockData);
+      repositoryMock.save.mockReturnValue({ ...mockData, id: -1 });
+
+      const register = await service.createRegister(-1, mockData);
+
+      expect(register).toHaveProperty('id');
+      expect(register).toMatchObject(mockData);
+      expect(repositoryMock.create).toBeCalledWith({
+        ...mockData,
+        userId: -1,
+      });
+      expect(repositoryMock.create).toBeCalledTimes(1);
+      expect(repositoryMock.save).toBeCalledWith(mockData);
+      expect(repositoryMock.save).toBeCalledTimes(1);
+    });
+  });
+
+  describe('when search register by user id', () => {
+    it('should find all the registers of a user by their id', async () => {
+      repositoryMock.find.mockReturnValue([mockData, mockData]);
+
+      const userId = -1;
+      const registers = await service.findRegistersByUser(userId);
+
+      expect(registers).toHaveLength(2);
+      expect(repositoryMock.find).toBeCalledWith({
+        order: { id: 'DESC' },
+        where: { userId },
+      });
+      expect(repositoryMock.find).toBeCalledTimes(1);
+    });
+  });
+
+  describe('when search all registers', () => {
+    it('should find all registers', async () => {
+      repositoryMock.find.mockReturnValue([mockData, mockData, mockData]);
+
+      const registers = await service.findAllRegisters();
+
+      expect(registers).toHaveLength(3);
+      expect(repositoryMock.find).toBeCalledTimes(1);
+    });
   });
 });
